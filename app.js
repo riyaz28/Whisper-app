0d2b1c9fc3d0e19d2186b4a7de3d7482d4b20367
riyaz28 <71028377+riyaz28@users.noreply.github.com>
2020-12-25 15:14:14 +0530

    Hahing keys

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
-var encrypt = require('mongoose-encryption');
+const md5=require("md5");
+
 
 const app=express();
 app.use(express.static("public"));
@@ -22,7 +23,7 @@ const userSchema=new mongoose.Schema({
 });
 
 // console.log(process.env.SECRET);
-userSchema.plugin(encrypt ,{secret:process.env.SECRET,encryptedFields:["password"]});
+
 
 const User=new mongoose.model("User",userSchema);
 
@@ -46,7 +47,7 @@ app.post("/register",function(req,res)
  const newUser=new User(
    {
      email:req.body.username,
-     password:req.body.password
+     password:md5(req.body.password)
    }
  );
  newUser.save(function(err)
@@ -64,7 +65,7 @@ app.post("/register",function(req,res)
 app.post("/login",function(req,res)
 {
 const username=req.body.username;
-const password=req.body.password;
+const password=md5(req.body.password);
 
 User.findOne({email:username},function(err,foundUser)
 {
