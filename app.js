bf380e16b762d3a543a8954881ea94990081eecb
riyaz28 <71028377+riyaz28@users.noreply.github.com>
2020-12-25 23:52:34 +0530

    adding passport.js for cookies,session + salting,hashing

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
 // const md5=require("md5");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+// const bcrypt = require("bcrypt");
+const session=require("express-session");
+const passport=require("passport");
+const passportLocalMongoose=require("passport-local-mongoose");
+// const saltRounds = 10;
 
 const app=express();
 app.use(express.static("public"));
@@ -17,22 +20,44 @@ app.use(bodyParser.urlencoded(
   }
 ));
 
+app.use(session(
+  {
+    secret:"OurLittleSecret",
+    resave:false,
+    saveUninitialized: true
+  }
+));
+
+app.use(passport.initialize());
+app.use(passport.session());
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true});
+mongoose.set("useCreateIndex",true);
 const userSchema=new mongoose.Schema({
   email:String,
   password:String
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 // console.log(process.env.SECRET);
-
-
 const User=new mongoose.model("User",userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.get("/",function(req,res)
 {
   res.render("home");
 });
 
+//logout request taken care
+app.get("/logout",function(req,res)
+{
+  req.logout();
+  res.redirect("/");
+});
 app.get("/login",function(req,res)
 {
   res.render("login");
@@ -43,54 +68,108 @@ app.get("/register",function(req,res)
   res.render("register");
 });
 
+app.get("/secrets",function(req,res)
+{
+  if(req.isAuthenticated())
+  {
+    res.render("secrets");
+  }
+  else
+  {
+   res.redirect("/login");
+  }
+});
+
 app.post("/register",function(req,res)
 {
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    const newUser=new User(
-      {
-        email:req.body.username,
-        password:hash
-      }
-    );
-    newUser.save(function(err)
-   {
-    if(err)
+  User.register({username:req.body.username},req.body.password,function(err,user)
+{
+  if(err)
+  {
+    console.log(err);
+    res.redirect("/register");
+  }
+  else
+  {
+    passport.authenticate("local")(req,res,function()
     {
-      console.log(err);
-    }
-    else
-    {
-      res.render("secrets");
-    }
-   });
+      res.redirect("secrets");
+    });
+  }
 });
 });
 app.post("/login",function(req,res)
 {
-const username=req.body.username;
-const password=req.body.password;
-
-User.findOne({email:username},function(err,foundUser)
+ const user=new User({
+   username:req.body.username,
+   password:req.body.password
+ });
+req.login(user,function(err)
 {
-if(err)
-{
-  console.log(err);
-}
-else
-{if(foundUser)
+  if(err)
   {
-    bcrypt.compare(password, foundUser.password, function(err, result) {
-      if(result==true)
-      {
-        res.render("secrets");
-      }
+    console.log(err);
+  }
+  else
+  {
+    passport.authenticate("local")(req,res,function()
+    {
+      res.redirect("secrets");
     });
   }
-}
 });
+
 });
 
 app.listen(3000,function()
 {
   console.log("Server is set up at port 3000");
 })
+
+
+
+//for bcrypt login route
+// const username=req.body.username;
+// const password=req.body.password;
+//
+// User.findOne({email:username},function(err,foundUser)
+// {
+// if(err)
+// {
+//   console.log(err);
+// }
+// else
+// {if(foundUser)
+//   {
+//     bcrypt.compare(password, foundUser.password, function(err, result) {
+//       if(result==true)
+//       {
+//         res.render("secrets");
+//       }
+//     });
+//   }
+// }
+// });
+
+
+
+//for bcrypt Register route
+// bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+//   const newUser=new User(
+//     {
+//       email:req.body.username,
+//       password:hash
+//     }
+//   );
+//   newUser.save(function(err)
+//  {
+//   if(err)
+//   {
+//     console.log(err);
+//   }
+//   else
+//   {
+//     res.render("secrets");
+//   }
+//  });
+// });
